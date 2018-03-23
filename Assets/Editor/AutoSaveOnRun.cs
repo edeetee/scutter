8655f1c93f31d17d41964aae377925adaa620293
Edward Taylor <edeetee@gmail.com>
2018-03-23 06:29:59 +0000

    Starting color generation

---
+++ Assets/Editor/AutoSaveOnRun.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public class AutosaveOnRun
+{
+	static AutosaveOnRun()
+	{
+		EditorApplication.playModeStateChanged += (state) =>
+		{
+			if(state == PlayModeStateChange.ExitingEditMode){
+				Debug.Log("Auto-Saving scene before entering Play mode: " + state);
+				AssetDatabase.SaveAssets();
+				EditorSceneManager.SaveOpenScenes();
+			}
+		};
+	}
+}
--- Assets/TakeControl.cs
+++ Assets/TakeControl.cs
@@ -5,19 +5,36 @@ using UnityEngine;
 public class TakeControl : MonoBehaviour {
 
 	float top = 0;
-	Rigidbody me;
 	Rigidbody target;
 
+	SpringJoint spring;
 	Bounds bounds;
 
 	// Use this for initialization
 	void Start () {
-		me = GetComponent<Rigidbody>();
-		var spring = GetComponent<SpringJoint>();
-		target = spring.connectedBody;
+		spring = GetComponent<SpringJoint>();
+		UpdateTarget(spring.connectedBody);
+	}
+
+	void Update() {
+		if(spring.connectedBody != target){
+			UpdateTarget(spring.connectedBody);
+		}
+	}
+
+	void UpdateTarget(Rigidbody target){
+		this.target = target;
 		
 		foreach (var collider in target.GetComponents<Collider>()) {
 			var newTop = collider.bounds.size.y / target.transform.localScale.y / 2;
+
+			// if(collider is BoxCollider)
+			// 	newTop = ((BoxCollider)collider).size.y/2 + ((BoxCollider)collider).center.y;
+			// else if(collider is CapsuleCollider)
+			// 	newTop = ((CapsuleCollider)collider).height/2 + ((CapsuleCollider)collider).center.y;
+			// else if(collider is SphereCollider)
+			// 	newTop = ((SphereCollider)collider).radius + ((SphereCollider)collider).center.y;
+			
 			if(top < newTop){
 				top = newTop;
 				bounds = collider.bounds;
@@ -32,15 +49,10 @@ public class TakeControl : MonoBehaviour {
 	{
 		if(UnityEditor.EditorApplication.isPlaying){
 			Gizmos.color = Color.cyan;
-			Gizmos.DrawWireSphere(target.transform.TransformPoint(new Vector3(0, top, 0)), 0.1f);
+			Gizmos.DrawWireSphere(target.transform.TransformPoint(new Vector3(0, top, 0)), 0.05f);
 			if(bounds != null){
 				Gizmos.DrawWireCube(target.position, bounds.size);
 			}
 		}
 	}
-
-	// void FixedUpdate () {
-	// 	var delta = me.position - (target.position + target.centerOfMass);
-	// 	target.AddForce(delta);
-	// }
 }
