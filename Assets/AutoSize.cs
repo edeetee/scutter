73fc9863c62a9db4ee87fa6498e4c973023ae38a
Edward Taylor <edeetee@gmail.com>
2018-04-12 12:42:11 +0000

    trying to auto size

---
+++ Assets/AutoSize.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class AutoSize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var chaperone = OpenVR.Chaperone;
+		HmdQuad_t quad_T = new HmdQuad_t();
+		chaperone.GetPlayAreaRect(ref quad_T);
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
